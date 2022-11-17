3a725f045ad135ee0bf29b4baa467f153eebd5eb
generativesystems <jtsr@btinternet.com>
2022-11-17 18:41:38 +0000

    init

---
+++ js/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    //canvas.position(0,0);
+  }
+  
+  function draw() {
+    background(220);
+  }
+
+//   This is just to create a background using p5.js to start with
